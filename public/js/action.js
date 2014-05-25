766f8f06a8f9a090158b571608d628cd7d539a5e
funnyjk <funnyjk@funnyjk-1000HE.(none)>
2014-05-25 18:23:04 -0400

    major changes starting

---
+++ public/js/action.js
@@ -0,0 +1,27 @@
+var urlLink = '192.168.1.20:5000';
+var socket = io.connect(urlLink);
+
+function forward() {
+  socket.emit('switch', "forward");
+  socket.emit('test', "forward");
+}
+function backward() {
+  socket.emit('switch', "backward");
+  socket.emit('test', "backward");
+}
+function left() {
+  socket.emit('switch', "left");
+  socket.emit('test', "left");
+}
+function right() {
+  socket.emit('switch', "right");
+  socket.emit('test', "right");
+}
+function stop() {
+  socket.emit('switch', "stop");
+  socket.emit('test', "stop");
+}
+function twitch(data) {
+  socket.emit('switch',data);
+  socket.emit('test',data);
+}
